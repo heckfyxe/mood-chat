5cf421487c08ef97c0698b9e4442040d49f316a0
heckfyxe <heckfyxe@gmail.com>
2019-01-06 19:39:46 +0500

    Crashlytics is added

--- app/src/main/java/com/heckfyxe/moodchat/App.kt
+++ app/src/main/java/com/heckfyxe/moodchat/App.kt
@@ -3,10 +3,14 @@ package com.heckfyxe.moodchat
 import android.content.Intent
 import android.widget.Toast
 import androidx.multidex.MultiDexApplication
+import com.crashlytics.android.Crashlytics
 import com.vk.sdk.VKAccessToken
 import com.vk.sdk.VKAccessTokenTracker
 import com.vk.sdk.VKSdk
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
+
+
 
 
 class App : MultiDexApplication() {
@@ -25,6 +29,8 @@ class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Fabric.with(this, Crashlytics())
 
         startKoin(this, listOf(koinModule))
 
